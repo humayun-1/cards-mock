1a75fd09a43ce7a332f13c8cea401286e87d76bb
salmankhan-txlabz <salman_khan@txlabz.com>
2024-07-28 23:34:33 +0500

    project initialization

---
+++ src/lib/collection.ts
@@ -0,0 +1,16 @@
+export const fetchCollection = () => {
+  /**
+   * Step 2: Instead of directly returning the collection, fetch it from http://localhost:8001/cards
+   */
+  return [
+    {
+      id: 26166,
+      player: {
+        firstname: 'Karim',
+        lastname: 'Benzema',
+        birthday: '1987-12-19T08:38:50.090Z',
+        image: 'https://images.fotmob.com/image_resources/playerimages/26166.png'
+      }
+    }
+  ];
+};
